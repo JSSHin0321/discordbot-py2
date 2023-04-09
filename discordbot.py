ddaa48b78da0d0e65f34aa8f299bf01f04123303
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-10 03:20:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -38,9 +38,6 @@ boss_list = [
 # 보스 목록과 예상 출현 시간을 저장하는 딕셔너리입니다.
 boss_timers = {}
 
-# 파티 정보를 저장하는 변수입니다.
-party = None
-
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}.')
@@ -65,59 +62,15 @@ async def on_message(message):
     if message.content == '보스맵 3':
         await message.channel.send('https://dszw1qtcnsa5e.cloudfront.net/community/20230404/29c555d6-eb4b-4674-8955-eae7d94b48d1/50%EB%B3%B4%EC%8A%A4.png?data-size=5311083')
 
-    # 파티 모집
-    elif message.content == '파티 모집':
-        global party
-        if party:
-            await message.channel.send('이미 진행 중인 파티 모집이 있습니다.')
-            return
 
-        party = {'leader': message.author, 'members': []}
-        await message.channel.send(f'{message.author.mention}님이 파티 모집을 시작했습니다. 참가하시려면 !참가 를 입력해주세요.')
-
-    # 참가 신청
-    elif message.content == '참가':
-        global party
-        if not party:
-            await message.channel.send('진행 중인 파티 모집이 없습니다.')
-            return
-
-        if len(party['members']) >= 5:
-            await message.channel.send('파티 인원이 꽉 찼습니다.')
-            return
-
-        if message.author in party['members']:
-            await message.channel.send('이미 참가한 상태입니다.')
-            return
-
-        party['members'].append(message.author)
-        await message.channel.send(f'{message.author.mention}님이 파티에 참가했습니다. ({len(party["members"])} / 5)')
-
-    # 파티 해산
-    elif message.content == f'{PREFIX}파티 해산':
-        global party
-        if not party:
-            await message.channel.send('진행 중인 파티 모집이 없습니다.')
-            return
-
-        if message.author != party['leader']:
-            await message.channel.send('파티 리더만 파티를 해산할 수 있습니다.')
-            return
-
-        members_str = ', '.join([member.mention for member in party['members']])
-        await message.channel.send(f'파티가 해산되었습니다. {members_str}')
-        party = None
-    
-    # "보스" 형태의 메시지에 대한 처리입니다.
+    # "보스" 라는 명령어가 입력되면 보스 리스트와 해당 보스의 예상 출현 시간을 출력합니다.
     elif message.content == '보스':
         # 예상 출현 시간이 빠른 순서대로 보스 목록을 정렬합니다.
         boss_timers_sorted = sorted(boss_timers.items(), key=lambda x: x[1]['time'] if x[1] else datetime.datetime.max)
         boss_list_sorted = [x[0] for x in boss_timers_sorted] + [x for x in boss_list if x not in boss_timers]
         # 보스 리스트와 예상 출현 시간을 문자열로 변환합니다.
-        author_name = message.author.display_name
-        boss_str = '\n'.join([f"[{boss}] 출현 예상 : {boss_timers[boss]['time'].strftime('%H:%M:%S')} ({author_name})" if boss in boss_timers else f"[{boss}] 출현 예상 : " for boss in boss_list_sorted])
+        boss_str = '\n'.join([f"[{boss}] 출현 예상 : {boss_timers[boss]['time'].strftime('%H:%M:%S')}" if boss in boss_timers else f"[{boss}] 출현 예상 : " for boss in boss_list_sorted])
         await message.channel.send(f"```{boss_str}```")
-
 
     # "보스 초기화" 형태의 메시지에 대한 처리입니다.
     elif len(message.content.split()) == 2 and message.content.split()[1] == '초기화':
@@ -177,7 +130,7 @@ async def on_message(message):
             # 예상 시간을 한국 시간으로 표기합니다.
             expected_time_str = expected_time_utc.strftime('%H:%M:%S')
             # 작성자 아이디를 가져와 메시지를 전송합니다.
-            author_name = message.author.display_name
+            author_name = message.author.name
             await message.channel.send(f"{args[0]} 보스 출현 시간이 변경되었습니다. / {author_name}")
 
 try:
