13264e1c97e1266424504931c7bb75ac499dc621
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-10 02:37:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -56,6 +56,12 @@ async def on_message(message):
     if message.content == '보스맵 1':
         await message.channel.send('https://dszw1qtcnsa5e.cloudfront.net/community/20230404/274c4eee-66f5-4a35-81d4-4b187667f333/40%EB%B3%B4%EC%8A%A4.png?data-size=5309107')
 
+    if message.content == '보스맵 2':
+        await message.channel.send('https://dszw1qtcnsa5e.cloudfront.net/community/20230404/716418d7-2576-43c3-9580-3d2bf2d77e58/45%EB%B3%B4%EC%8A%A4.png?data-size=5332128')
+
+    if message.content == '보스맵 3':
+        await message.channel.send('https://dszw1qtcnsa5e.cloudfront.net/community/20230404/29c555d6-eb4b-4674-8955-eae7d94b48d1/50%EB%B3%B4%EC%8A%A4.png?data-size=5311083')
+
 
     # "보스" 라는 명령어가 입력되면 보스 리스트와 해당 보스의 예상 출현 시간을 출력합니다.
     elif message.content == '보스':
