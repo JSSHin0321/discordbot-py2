8082111c7686044feda90058961e6a7fde58a232
JSSHin0321 <130244820+JSSHin0321@users.noreply.github.com>
2023-04-10 17:05:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,7 +24,7 @@ boss_list = [
     "로드리고",
     "프랜신",
     "루바란",
-    "볼테트",
+    "볼테르",
     "라크다르",
     "이올라",
     "기드온",
@@ -68,13 +68,12 @@ async def on_message(message):
             "보스맵 2": "45보스맵을 출력합니다.",
             "보스맵 3": "50보스맵을 출력합니다.",
             "보스": "보스 목록과 예상 출현 시간을 출력합니다.",
+            "보스이름 컷": "해당 보스의 출현 시간을 등록합니다.",
             "보스이름 초기화": "해당 보스의 예상 출현 시간을 초기화합니다.",
             "보스이름 시간": "해당 보스의 예상 출현 시간을 변경합니다."
         }
         command_str = "\n".join([f"{k}: {v}" for k, v in command_dict.items()])
         await message.channel.send(f"```{command_str}```")
-
-
 
 
     # "보스" 라는 명령어가 입력되면 보스 리스트와 해당 보스의 예상 출현 시간을 출력합니다.
@@ -83,9 +82,13 @@ async def on_message(message):
         boss_timers_sorted = sorted(boss_timers.items(), key=lambda x: x[1]['time'] if x[1] else datetime.datetime.max)
         boss_list_sorted = [x[0] for x in boss_timers_sorted] + [x for x in boss_list if x not in boss_timers]
         # 보스 리스트와 예상 출현 시간을 문자열로 변환합니다.
-        author_name = message.author.display_name
-        boss_str = '\n'.join([f"[{boss}] 출현 예상 : {boss_timers[boss]['time'].strftime('%H:%M:%S')} ({author_name})" if boss in boss_timers else f"[{boss}] 출현 예상 : " for boss in boss_list_sorted])
+        boss_str = '\n'.join([f"[{boss}] 출현 예상 : {boss_timers[boss]['time'].strftime('%H:%M:%S')}" if boss in boss_timers else f"[{boss}] 출현 예상 : " for boss in boss_list_sorted])
         await message.channel.send(f"```{boss_str}```")
+
+
+
+
+
 
     # "보스 초기화" 형태의 메시지에 대한 처리입니다.
     elif len(message.content.split()) == 2 and message.content.split()[1] == '초기화':
@@ -100,7 +103,7 @@ async def on_message(message):
     # '컷'이 들어간 메시지에 대한 처리입니다.
     elif message.content.endswith('컷'):
         # 보스 이름을 추출합니다.
-        boss_name = message.content[:-1].strip()
+        boss_name = message.content.split()[0]
 
         # 보스 이름이 존재하지 않는 경우, 에러 메시지를 전송합니다.
         if boss_name not in boss_list:
@@ -116,9 +119,9 @@ async def on_message(message):
             boss_timers[boss_name]['time'] += datetime.timedelta(hours=9)
             # 예상 시간을 한국 시간으로 표기합니다.
             expected_time_str = boss_timers[boss_name]['time'].strftime('%H:%M:%S')
-            # 작성자 아이디를 가져와 메시지를 전송합니다.
-            author_name = message.author.display_name
-            await message.channel.send(f"{boss_name} 보스 타이머가 초기화되었고, {expected_time_str} 에 다시 출현합니다. / {author_name}")
+            await message.channel.send(f"{boss_name} 보스 타이머가 초기화되었습니다.")
+
+
 
     # "보스이름 시간" 형태의 메시지에 대한 처리입니다.
     elif len(message.content.split()) == 2:
@@ -144,11 +147,12 @@ async def on_message(message):
             }
             # 예상 시간을 한국 시간으로 표기합니다.
             expected_time_str = expected_time_utc.strftime('%H:%M:%S')
-            # 작성자 아이디를 가져와 메시지를 전송합니다.
-            author_name = message.author.display_name
-            await message.channel.send(f"{args[0]} 보스 출현 시간이 변경되었습니다. / {author_name}")
+            await message.channel.send(f"{args[0]} 보스 출현 시간이 변경되었습니다.")
+
 
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
     print("Improper token has been passed.")
+
+
